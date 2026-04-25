da40f82ab539e1b19859a77c12aed059aa5d26d9
crevanth <213225024+crevanth@users.noreply.github.com>
2026-04-25 19:16:19 +0530

    Update setup.sh

--- .github/workflows/setup.sh
+++ .github/workflows/setup.sh
@@ -2,16 +2,16 @@ function setup() {
     sudo apt update
     sudo apt install -y bc bash git-core gnupg build-essential \
         zip curl make automake autogen autoconf autotools-dev \
-        libtool shtool python3 m4 gcc libtool zlib1g-dev \
+        libtool shtool python3 m4 gcc zlib1g-dev \
         flex bison libssl-dev libelf-dev device-tree-compiler
 
-    # Proton Clang — patched for kernel 4.9
-    git clone https://github.com/kdrag0n/proton-clang \
-        --depth=1 clang
-
-    # GCC as linker fallback
+    # GCC aarch64 toolchain
     git clone https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9 \
-        -b android-10.0.0_r47 --depth=1 gcc
+        -b android-10.0.0_r47 --depth=1 gcc64
+
+    # GCC arm toolchain (needed for vdso)
+    git clone https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9 \
+        -b android-10.0.0_r47 --depth=1 gcc32
 
     # AnyKernel3
     git clone https://github.com/crsvt/AnyKernel3
