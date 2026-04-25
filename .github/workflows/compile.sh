448043ffab9ca2d7ad48286785e2ac61935b7f06
crevanth <213225024+crevanth@users.noreply.github.com>
2026-04-25 18:41:01 +0530

    Update compile.sh

--- .github/workflows/compile.sh
+++ .github/workflows/compile.sh
@@ -2,23 +2,16 @@
 ROOT_DIR=$(pwd)
 
 function compile() {
-    # export the arch
     export ARCH=arm64
+    export PATH="${ROOT_DIR}/clang/bin:${ROOT_DIR}/gcc/bin:${PATH}"
 
-    # make clang/gcc PATH available
-    PATH=:"${ROOT_DIR}/clang/bin:${PATH}:${ROOT_DIR}/gcc/bin:${PATH}"
-
-    # make the config
     make O=out onc_defconfig
 
-    # Start the build
     make -j$(nproc --all) O=out \
-                    ARCH=arm64 \
-                    CC=clang \
-                    CLANG_TRIPLE=aarch64-linux-gnu- \
-                    CROSS_COMPILE=aarch64-linux-android-
+        ARCH=arm64 \
+        CC=clang \
+        CLANG_TRIPLE=aarch64-linux-gnu- \
+        CROSS_COMPILE=aarch64-linux-android-
 }
 
-
-# Starts here
 compile
