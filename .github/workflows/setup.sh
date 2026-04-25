3fec5475c4787becdcc8058a026e9086050a0e77
crevanth <213225024+crevanth@users.noreply.github.com>
2026-04-25 18:41:15 +0530

    Update setup.sh

--- .github/workflows/setup.sh
+++ .github/workflows/setup.sh
@@ -1,13 +1,14 @@
-## Script to setup the environment and download the required tools
+# Setup environment and download required tools
+function setup() {
+    sudo apt update
+    sudo apt install -y bc bash git-core gnupg build-essential \
+        zip curl make automake autogen autoconf autotools-dev \
+        libtool shtool python3 m4 gcc libtool zlib1g-dev \
+        flex bison libssl-dev libelf-dev
 
-function setup () {
-    # Install dependencies
-    sudo apt install bc bash git-core gnupg build-essential \
-        zip curl make automake autogen autoconf autotools-dev libtool shtool python \
-        m4 gcc libtool zlib1g-dev flex bison libssl-dev
-    
-    # Download gcc
-    git clone https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9 -b android-10.0.0_r47 --depth=1 gcc
+    # Download GCC
+    git clone https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9 \
+        -b android-10.0.0_r47 --depth=1 gcc
 
     # Download Clang
     wget https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/refs/tags/android-11.0.0_r37/clang-r365631c.tar.gz
@@ -17,8 +18,6 @@ function setup () {
 
     # Clone AnyKernel3
     git clone https://github.com/crsvt/AnyKernel3
-
 }
 
-# Starts here
 setup
