3da833ffc6c1bf6ea7d0d1f2a5c321ed7874f867
crevanth <213225024+crevanth@users.noreply.github.com>
2026-04-26 20:26:49 +0530

    Update setup.sh

--- .github/workflows/setup.sh
+++ .github/workflows/setup.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 function setup() {
     sudo apt update
     sudo apt install -y bc bash git-core gnupg build-essential \
@@ -5,16 +6,12 @@ function setup() {
         libtool shtool python3 m4 gcc zlib1g-dev \
         flex bison libssl-dev libelf-dev device-tree-compiler
 
-    # LineageOS GCC aarch64
     git clone https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9 \
         -b cm-14.1 --depth=1 gcc64
 
-    # LineageOS GCC arm32 (for vdso)
     git clone https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_arm_arm-linux-androideabi-4.9 \
         -b cm-14.1 --depth=1 gcc32
 
-    # AnyKernel3
     git clone https://github.com/crsvt/AnyKernel3
 }
-
 setup
